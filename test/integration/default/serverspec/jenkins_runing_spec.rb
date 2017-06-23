8bd61fb0861db92e32e17295646a8e7dca3293f3
hgmiguel <hgmiguel@gmail.com>
2017-06-22 21:01:58 -0500

    Fix test, added ssh backend to serverspec

--- test/integration/default/serverspec/jenkins_runing_spec.rb
+++ test/integration/default/serverspec/jenkins_runing_spec.rb
@@ -1,7 +1,16 @@
-require 'serverspec'
+require 'rubygems'
+require 'bundler/setup'
 
-# Required by serverspec
-set :backend, :exec
+require 'serverspec'
+require 'pathname'
+require 'net/ssh'
+
+RSpec.configure do |config|
+    set :host, ENV['KITCHEN_HOSTNAME']
+    set :ssh_options, :user => ENV['KITCHEN_USERNAME'], :port => ENV['KITCHEN_PORT'], :paranoid => false, :host_key => 'ssh-rsa', :keys => [ ENV['KITCHEN_SSH_KEY'] ] if ENV['KITCHEN_SSH_KEY']
+    set :backend, :ssh
+    set :request_pty, true
+end
 
 describe port(8080) do
     it { should be_listening }
